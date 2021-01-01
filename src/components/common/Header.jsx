c4544a6c0d43cedac57a18ff79f16b42dfa09f9e
Arthur <arthurdelbeke@smartness.co>
2021-01-01 15:50:32 +0100

    🎨 Implement the header‍

---
+++ src/components/common/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.jsx.css'
+
+export const Header = ({}) => {
+    return (
+        <div className="header">
+            <h1 className="title">Progyear</h1>
+        </div>
+    )
+}
